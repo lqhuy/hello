0e3944000c328c31250b634ce90f36e744f1ce0a
lqhuy <54682489+lqhuy@users.noreply.github.com>
2019-08-29 12:56:00 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Cory Shoff and Huy Le
+ * Date: 2019/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Cory and Huy\n");
+
+  return 0;
+}
